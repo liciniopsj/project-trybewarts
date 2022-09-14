2bab1dd960b5cedba1660cb8c94150db884255b8
Andrew <andrewmll94@gmail.com>
2022-09-14 17:48:21 -0300

    Requisito 3 em progresso

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const email = 'tryber@teste.com';
 const password = '123456';
-const emailField = document.getElementById('email-field').value;
-const passwordField = document.getElementById('password-field').value;
+const emailField = document.getElementById('email-field');
+const passwordField = document.getElementById('password-field');
 const loginButton = document.getElementById('loginButton');
 
-function compare() {
-  if (email === emailField) return true;
-  return false;
-}
+// function compare() {
+//   if (email === emailField) return true;
+//   return false;
+// }
 
-function loginAlert(confirmation) {
-  if (confirmation === true) {
+// function loginAlert(confirmation) {
+//   if (confirmation === true) {
+//     alert('Olá, Tryber!');
+//   } else {
+//     alert('Email ou senha inválidos');
+//   }
+// }
+
+// loginButton.addEventListener('click', loginAlert(compare()));
+
+loginButton.addEventListener('click', function() {
+  if (emailField === 'tryber@teste.com') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos');
   }
-}
-
-loginButton.addEventListener('click', loginAlert(compare()));
-
-// loginAlert(compare());
+})
