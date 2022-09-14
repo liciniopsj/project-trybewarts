920205e892bcfc2a54c1f3726804d55138cefe0a
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-09-14 17:44:07 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-const email = 'tryber@teste.com';
-const password = '123456';
-const emailField = document.getElementById('email-field').value;
-const passwordField = document.getElementById('password-field').value;
 const loginButton = document.getElementById('loginButton');
+let isAuthorized = false;
 
 function compare() {
-  if (email === emailField) return true;
-  return false;
+  const emailField = document.getElementById('email-field').value;
+  const passwordField = document.getElementById('password-field').value;
+
+  if (emailField === 'tryber@teste.com' && passwordField === '123456') {
+    isAuthorized = true;
+  } else {
+    isAuthorized = false;
+  }
 }
 
 function loginAlert(confirmation) {
   if (confirmation === true) {
-    alert('Olá, Tryber!');
+   alert('Olá, Tryber!');
   } else {
-    alert('Email ou senha inválidos');
+   alert('Email ou senha inválidos');
   }
 }
 
-loginButton.addEventListener('click', loginAlert(compare()));
 
-// loginAlert(compare());
+loginButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  compare();
+  loginAlert(isAuthorized);
+});
