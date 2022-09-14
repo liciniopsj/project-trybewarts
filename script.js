88d1e2ed228175204973f92ef9b9a9f51dc7814f
Andrew <andrewmll94@gmail.com>
2022-09-14 20:49:29 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const loginButton = document.getElementById('loginButton');
+const checkbox = document.getElementById('agreement');
 let isAuthorized = false;
 
 function compare() {
@@ -25,3 +26,14 @@ loginButton.addEventListener('click', (event) => {
   compare();
   loginAlert(isAuthorized);
 });
+
+submitButton.addEventListener('click', function(event) {
+  event.preventDefault();
+})
+
+checkbox.onchange = function() {
+  const submitButton = document.getElementById('submit-btn');
+  if (checkbox.checked) {
+    submitButton.disabled = !!this.checked;
+  }
+}
