940cbd2aa07eb5d27318aa5e9ceee54d8471948d
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-09-14 21:05:18 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const loginButton = document.getElementById('loginButton');
 const checkbox = document.getElementById('agreement');
+const submitButton = document.getElementById('submit-btn');
 let isAuthorized = false;
 
 function compare() {
@@ -27,13 +28,10 @@ loginButton.addEventListener('click', (event) => {
   loginAlert(isAuthorized);
 });
 
-submitButton.addEventListener('click', function(event) {
-  event.preventDefault();
-})
-
 checkbox.onchange = function() {
-  const submitButton = document.getElementById('submit-btn');
-  if (checkbox.checked) {
-    submitButton.disabled = !!this.checked;
+  if(this.checked){
+    submitButton.disabled = false;
+} else {
+    submitButton.disabled = true;
   }
 }
