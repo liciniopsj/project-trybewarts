8d62277f04116fb193ad44af50e3a0564d79ad2f
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-09-16 20:59:49 -0300

    req 21

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const checkbox = document.getElementById('agreement');
 const submitButton = document.getElementById('submit-btn');
 const textArea = document.getElementById('textarea');
 const counter = document.getElementById('counter');
+const form = document.getElementById('evaluation-form');
+const formData = document.getElementById('form-data');
 counter.innerText = 500;
 let isAuthorized = false;
 
@@ -42,4 +44,62 @@ checkbox.onchange = function enableBtn() {
 textArea.addEventListener('input', () => {
   const textAreaLength = textArea.value.length;
   counter.innerText = 500 - textAreaLength;
+});
+
+function getFullName() {
+  const studentName = document.getElementById('input-name').value;
+  const studentLastName = document.getElementById('input-lastname').value;
+  document.getElementById('studentFullName').innerText = `Nome: ${studentName} ${studentLastName}`;
+}
+
+function getMailAndHome() {
+  const studentEmail = document.getElementById('studentEmail');
+  const studentHouse = document.getElementById('studentHouse')
+  studentEmail.innerText = `Email: ${document.getElementById('input-email').value}`;
+  studentHouse.innerText = `Casa: ${document.getElementById('house').value}`;
+}
+
+function getSubjectArray() {
+  const subjectArray = document.querySelectorAll('input[name=subject]:checked');
+  const subjectArrayValues = [];
+  for (let i = 0; i < subjectArray.length; i += 1) {
+    subjectArrayValues[i] = subjectArray[i].value;
+  }
+  const subjectString = subjectArrayValues.join(', ');
+  const studentSubjects = document.getElementById('studentSubjects');
+  studentSubjects.innerText = `Matérias: ${subjectString}`;
+}
+
+function getRating() {
+  const ratingArray = document.getElementsByName('rate');
+  const studentEval = document.getElementById('studentEval');
+  let formEvaluation = 0;
+  for (let i = 0; i < ratingArray.length; i += 1) {
+    if (ratingArray[i].checked === true) formEvaluation = ratingArray[i].value;
+  }
+  studentEval.innerText = `Avaliação: ${formEvaluation}`;
+}
+
+function getFamily() {
+  const family = document.querySelector('input[name="family"]:checked').value;
+  const studentFamiliy = document.getElementById('studentFamiliy');
+  studentFamiliy.innerText = `Família: ${family}`;
+}
+
+function getCommentary() {
+  const commentary = document.getElementById('textarea');
+  const studentComment = document.getElementById('studentComment');
+  studentComment.innerText = `Observações: ${commentary.value}`;
+}
+
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  getFullName();
+  getMailAndHome();
+  getRating();
+  getFamily();
+  getSubjectArray();
+  getCommentary();
+  form.style.display = 'none';
+  formData.style.display = 'flex';
 });
